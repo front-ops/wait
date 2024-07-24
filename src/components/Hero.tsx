52003d2f1a707f2d8b1158e6a7fa9bc9956922bf
egorxvc <egorxvc@gmail.com>
2024-07-24 16:30:32 +0300

    init

---
+++ src/components/Hero.tsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+
+export default function Hero() {
+    return (
+        <Spline scene="https://prod.spline.design/kB0-zel8HQGgpSWr/scene.splinecode" />
+    );
+}
